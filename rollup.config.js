f43b2f2ac97b4cb0c80c3f5e598e95b46b657b5a
Arne Goedeke <arne@deuso.de>
2020-06-26 10:07:46 +0200

    added missing rollup config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    input: 'src/index.browser.esm.js',
+    output: {
+      file: 'dist/ember-plus.browser.js',
+      format: 'umd',
+      name: 'EmberPlus',
+    }
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/ember-plus.js',
+      format: 'umd',
+      name: 'EmberPlus',
+    }
+  },
+];
