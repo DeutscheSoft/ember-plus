33a9aa5d046c592a135c77fdfd74eb848b977cc7
Arne Goedeke <arne@deuso.de>
2020-10-27 09:20:02 +0100

    printTree: only print changed values

--- bin/printTree.js
+++ bin/printTree.js
@@ -5,11 +5,15 @@ async function connect(options) {
   connection.batch = 1;
   const device = new Device(connection);
 
+  const lastValues = new Map();
+
   const handleNode = (node) => {
     node.children.forEach((child) => {
       if (child instanceof Parameter) {
         const path = child.identifierPath.join('/');
         device.observeProperty(child, 'value', (value) => {
+          if (value === lastValues.get(path)) return;
+          lastValues.set(path, value);
           console.log('%o -> %o', path, value);
         });
       } else {
@@ -25,6 +29,12 @@ const options = {
   host: process.argv[2],
   port: parseInt(process.argv[3]),
 };
+
+if (!(options.port > 0 && options.port <= 0xffff) ||
+    !(typeof options.host === 'string' && options.host.length > 0)) {
+  console.error('Usage: %s %s <IP> <PORT>', process.argv[0], process.argv[1]);
+  process.exit(1);
+}
 
 console.log('Connecting to', options);
 
