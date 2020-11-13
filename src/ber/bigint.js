9fc1f4a14cd2c6854bfc270895258a8d90eadd64
Arne Goedeke <arne@deuso.de>
2020-11-13 12:06:21 +0100

    ber: avoid bigint syntax

--- src/ber/bigint.js
+++ src/ber/bigint.js
@@ -1,6 +1,6 @@
 export const HAS_BIGINT = typeof BigInt !== 'undefined';
-export const UINT64_MAX = HAS_BIGINT && (1n << 64n) - 1n;
-export const INT64_MAX = HAS_BIGINT && UINT64_MAX >> 1n;
-export const INT64_MIN = HAS_BIGINT && -INT64_MAX - 1n;
-export const INT56_MAX = HAS_BIGINT && (1n << 55n) - 1n;
-export const INT56_MIN = HAS_BIGINT && -INT56_MAX - 1n;
+export const UINT64_MAX = HAS_BIGINT && (BigInt(1) << BigInt(64)) - BigInt(1);
+export const INT64_MAX = HAS_BIGINT && UINT64_MAX >> BigInt(1);
+export const INT64_MIN = HAS_BIGINT && -INT64_MAX - BigInt(1);
+export const INT56_MAX = HAS_BIGINT && (BigInt(1) << BigInt(55)) - BigInt(1);
+export const INT56_MIN = HAS_BIGINT && -INT56_MAX - BigInt(1);
